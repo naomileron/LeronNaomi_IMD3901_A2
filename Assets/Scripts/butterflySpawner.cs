963e9177e1612d7fbf40a5794587f253505cc8be
Naomi <leronnaomi@gmail.com>
2026-01-27 21:29:51 -0500

    Butterfly Spawn
    Got basic spawning behaviour to work

--- Assets/Scripts/butterflySpawner.cs
+++ Assets/Scripts/butterflySpawner.cs
@@ -3,13 +3,22 @@ using UnityEngine;
 public class butterflySpawner : MonoBehaviour
 {
     public GameObject butterflyPrefab;
-   
+    public int butterflyCount = 10;
+    public float spawnRadius = 10f;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GameObject butterfly = Instantiate(butterflyPrefab, transform.position, transform.rotation);
-        butterfly.GetComponent<butterflyWander>().startPos = this.gameObject;
+        for (int i = 0; i < butterflyCount; i++)
+        {
+            Vector3 spawnPos = transform.position +
+                               Random.insideUnitSphere * spawnRadius;
+
+            spawnPos.y = Mathf.Abs(spawnPos.y);
+
+            Instantiate(butterflyPrefab, spawnPos, Quaternion.identity);
+        }
     }
 
     // Update is called once per frame

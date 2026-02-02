16217b7901a2befe9ce27b1f18b88e2528dc5acb
Naomi <leronnaomi@gmail.com>
2026-02-02 02:00:31 -0500

    VR Hands
    Added hands to VR mode. A lot of bugs currently but basic behaviour and animation set up is there.

---
+++ Assets/Scripts/VRAnimatedHands.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class VRAnimatedHands : MonoBehaviour
+{
+    public InputActionProperty grabValue;
+    public InputActionProperty releaseValue;
+
+    public Animator handAnimator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float grab = grabValue.action.ReadValue<float>();
+        float release = releaseValue.action.ReadValue<float>();
+
+        handAnimator.SetTrigger("Grab");
+        handAnimator.SetTrigger("Release");
+    }
+}
